b5af5d784cfe6e975ea5be860bb0d1c05eb4f5c0
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-10-24 14:21:10 -0400

    add fortunes list

--- fortuneteller/randomfortune/views.py
+++ fortuneteller/randomfortune/views.py
@@ -1,5 +1,26 @@
+# Imports
 from django.shortcuts import render
+import random
 
-# Create your views here.
+
+# List of fortunes to be returned at random
+fortuneList = [
+   "All will go well with your new project.",
+   "If you continually give, you will continually have.",
+   "Self-knowledge is a life long process.",
+   "You are busy, but you are happy.",
+   "Your abilities are unparalleled.",
+   "Those who care will make the effort.",
+   "Now is the time to try something new.",
+   "Miles are covered one step at a time.",
+   "Don’t just think, act!",
+   "Get Shrekt!"
+]
+
+# Views
 def fortune(request):
-  return render(request, "randomfortune/fortune.html")
+  fortune = random.choice(fortuneList)
+  context = {
+    "fortune": fortune
+  }
+  return render(request, "randomfortune/fortune.html", context)
